7f39327edb5f44a8f35b7aa17db6e73974f2f3aa
mehran <taghianmehran@gmail.com>
2022-01-15 15:21:13 -0700

    Saving experiment data added

--- scripts/v1.sh
+++ scripts/v1.sh
@@ -7,8 +7,9 @@ MAIN_FILE="/local/melco3/taghianj/SAC_GCN/main.py"
 tmux new-session -d -s v1-3 "CUDA_VISIBLE_DEVICES=1 python $MAIN_FILE --env-name FetchReachEnv-v1 --exp-type standard --automatic_entropy_tuning True --num_steps 250000 --start_steps 10000 -dsf 100 --cuda --seed 3"
 tmux new-session -d -s v1-4 "CUDA_VISIBLE_DEVICES=3 python $MAIN_FILE --env-name FetchReachEnv-v1 --exp-type standard --automatic_entropy_tuning True --num_steps 250000 --start_steps 10000 -dsf 100 --cuda --seed 4"
 tmux new-session -d -s v1-5 "CUDA_VISIBLE_DEVICES=4 python $MAIN_FILE --env-name FetchReachEnv-v1 --exp-type standard --automatic_entropy_tuning True --num_steps 250000 --start_steps 10000 -dsf 100 --cuda --seed 5"
-tmux new-session -d -s v1-6 "CUDA_VISIBLE_DEVICES=6 python $MAIN_FILE --env-name FetchReachEnv-v1 --exp-type standard --automatic_entropy_tuning True --num_steps 250000 --start_steps 10000 -dsf 100 --cuda --seed 6"
-tmux new-session -d -s v1-7 "CUDA_VISIBLE_DEVICES=4 python $MAIN_FILE --env-name FetchReachEnv-v1 --exp-type standard --automatic_entropy_tuning True --num_steps 250000 --start_steps 10000 -dsf 100 --cuda --seed 7"
-tmux new-session -d -s v1-8 "CUDA_VISIBLE_DEVICES=6 python $MAIN_FILE --env-name FetchReachEnv-v1 --exp-type standard --automatic_entropy_tuning True --num_steps 250000 --start_steps 10000 -dsf 100 --cuda --seed 8"
-tmux new-session -d -s v1-9 "CUDA_VISIBLE_DEVICES=6 python $MAIN_FILE --env-name FetchReachEnv-v1 --exp-type standard --automatic_entropy_tuning True --num_steps 250000 --start_steps 10000 -dsf 100 --cuda --seed 9"
+tmux new-session -d -s v1-6 "CUDA_VISIBLE_DEVICES=4 python $MAIN_FILE --env-name FetchReachEnv-v1 --exp-type standard --automatic_entropy_tuning True --num_steps 250000 --start_steps 10000 -dsf 100 --cuda --seed 6"
+tmux new-session -d -s v1-7 "CUDA_VISIBLE_DEVICES=6 python $MAIN_FILE --env-name FetchReachEnv-v1 --exp-type standard --automatic_entropy_tuning True --num_steps 250000 --start_steps 10000 -dsf 100 --cuda --seed 7"
+tmux new-session -d -s v1-8 "CUDA_VISIBLE_DEVICES=6
+ python $MAIN_FILE --env-name FetchReachEnv-v1 --exp-type standard --automatic_entropy_tuning True --num_steps 250000 --start_steps 10000 -dsf 100 --cuda --seed 8"
+tmux new-session -d -s v1-9 "CUDA_VISIBLE_DEVICES=7 python $MAIN_FILE --env-name FetchReachEnv-v1 --exp-type standard --automatic_entropy_tuning True --num_steps 250000 --start_steps 10000 -dsf 100 --cuda --seed 9"
 
