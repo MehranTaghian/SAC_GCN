fe6fdf5526844143f150c72eaaea1bcf738048ff
mehran <taghianmehran@gmail.com>
2022-03-31 18:20:25 -0600

    fetchreach changed to use joint velocities as actions

--- CustomGymEnvs/envs/fetchreach/CustomFetchReach/utils.py
+++ CustomGymEnvs/envs/fetchreach/CustomFetchReach/utils.py
@@ -64,7 +64,6 @@ def mocap_set_action(sim, action, joint_list=None):
             # sim.data.set_joint_qpos(joint_list[j].attrib['name'], joint_qpos + delta)
 
             # second type of giving action by joint velocity
-            print(action[j])
             sim.data.set_joint_qvel(joint_list[j].attrib['name'], action[j])
 
     # END MODIFICATION
