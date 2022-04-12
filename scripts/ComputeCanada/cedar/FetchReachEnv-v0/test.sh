e92302201cd43787ebbedb6163f2974b6f3b6e67
mehran <taghianmehran@gmail.com>
2022-04-12 17:33:55 -0600

    fetchreach v3 added

---
+++ scripts/ComputeCanada/cedar/FetchReachEnv-v0/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#!/bin/bash
+#SBATCH --account=def-someuser
+#SBATCH --gpus-per-node=1
+#SBATCH --mem=20G               # memory per node
+#SBATCH --time=00:05:00
+#SBATCH --job-name="FetchReachEnv-v0"
+#SBATCH --account=def-zaiane
+#SBATCH --mail-user=taghianj@ualberta.ca
+#SBATCH --mail-type=ALL
+
+parallel test.txt
