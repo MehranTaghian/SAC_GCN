a1fb0f5024cbef7198f4b8dffa46e43c44c4981b
mehran <taghianmehran@gmail.com>
2022-04-22 10:53:22 -0600

    SAC with normal environment and selected joints added

--- CustomGymEnvs/__init__.py
+++ CustomGymEnvs/__init__.py
@@ -19,43 +19,43 @@ register(
 
 register(
     id="FetchReachEnvGraph-v0",
-    entry_point="CustomGymEnvs.graph_envs.fetchreach.sh.CustomFetchReach.fetch.reach:FetchReachEnv",
+    entry_point="CustomGymEnvs.graph_envs.fetchreach.CustomFetchReach.fetch.reach:FetchReachEnv",
     max_episode_steps=50,
 )
 
 register(
     id="FetchReachEnvGraph-v1",
-    entry_point="CustomGymEnvs.graph_envs.fetchreach.sh.FetchReach_BrokenShoulderLift.fetch.reach:FetchReachEnv",
+    entry_point="CustomGymEnvs.graph_envs.fetchreach.FetchReach_BrokenShoulderLift.fetch.reach:FetchReachEnv",
     max_episode_steps=50,
 )
 
 register(
     id="FetchReachEnvGraph-v2",
-    entry_point="CustomGymEnvs.graph_envs.fetchreach.sh.FetchReach_ElbowFlexNoisyMovement.fetch.reach:FetchReachEnv",
+    entry_point="CustomGymEnvs.graph_envs.fetchreach.FetchReach_ElbowFlexNoisyMovement.fetch.reach:FetchReachEnv",
     max_episode_steps=50,
 )
 
 register(
     id="FetchReachEnvGraph-v3",
-    entry_point="CustomGymEnvs.graph_envs.fetchreach.sh.FetchReach_WristFlexNoisyMovement.fetch.reach:FetchReachEnv",
+    entry_point="CustomGymEnvs.graph_envs.fetchreach.FetchReach_WristFlexNoisyMovement.fetch.reach:FetchReachEnv",
     max_episode_steps=50,
 )
 
 register(
     id="FetchReachEnvGraph-v4",
-    entry_point="CustomGymEnvs.graph_envs.fetchreach.sh.FetchReach_ShoulderLiftNoisyMovement.fetch.reach:FetchReachEnv",
+    entry_point="CustomGymEnvs.graph_envs.fetchreach.FetchReach_ShoulderLiftNoisyMovement.fetch.reach:FetchReachEnv",
     max_episode_steps=50,
 )
 
 register(
     id="FetchReachEnvGraph-v5",
-    entry_point="CustomGymEnvs.graph_envs.fetchreach.sh.FetchReach_JointVel.fetch.reach:FetchReachEnv",
+    entry_point="CustomGymEnvs.graph_envs.fetchreach.FetchReach_JointVel.fetch.reach:FetchReachEnv",
     max_episode_steps=50,
 )
 
 register(
     id="FetchReachEnvGraph-v6",
-    entry_point="CustomGymEnvs.graph_envs.fetchreach.sh.FetchReach_Occluded.fetch.reach:FetchReachEnv",
+    entry_point="CustomGymEnvs.graph_envs.fetchreach.FetchReach_Occluded.fetch.reach:FetchReachEnv",
     max_episode_steps=50,
 )
 
@@ -63,7 +63,7 @@ register(
 
 register(
     id="FetchReachEnv-v0",
-    entry_point="CustomGymEnvs.envs.fetchreach.sh.CustomFetchReach.fetch.reach:FetchReachEnv",
+    entry_point="CustomGymEnvs.envs.fetchreach.CustomFetchReach.fetch.reach:FetchReachEnv",
     max_episode_steps=50,
 )
 
