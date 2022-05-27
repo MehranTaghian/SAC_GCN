ec1dfdde5e12002d04e5b353e999d3f11a62cbd3
mehran <taghianmehran@gmail.com>
2022-05-27 10:42:08 -0600

    number of threads for each run reduced to 2

---
+++ scripts/melco/copy.sh
@@ -0,0 +1,8 @@
+# MELCO 1
+rsync -a taghianj@melco.cs.ualberta.ca:/local/melco3/taghianj/SAC_GCN/Data/* /local/melco3/taghianj/Data_SAC_GCN
+
+#MELCO 2
+rsync -a taghianj@melco2.cs.ualberta.ca:/home/taghianj/Documents/SAC_GCN/Data/* /local/melco3/taghianj/Data_SAC_GCN
+
+# MEHRAN
+rsync -a taghianj@mehran.cs.ualberta.ca:/home/taghianj/Documents/SAC_GCN/Data/* /local/melco3/taghianj/Data_SAC_GCN
