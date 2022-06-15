d901df6888fb16f23d8c76bee618772ff21c6468
mehran <taghianmehran@gmail.com>
2022-06-15 10:29:15 -0600

    experiments scripts updated

---
+++ scripts/ComputeCanada/cedar/HalfCheetah/standard.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=p100:4
+#SBATCH --ntasks-per-node=24
+#SBATCH --exclusive
+#SBATCH --mem=125G
+#SBATCH --time=6-00:00
+#SBATCH --job-name="FetchReachEnv-v0"
+#SBATCH --account=def-zaiane
+#SBATCH --output=FetchReachEnv-v0-standard-%j.out
+#SBATCH --mail-user=taghianj@ualberta.ca
+#SBATCH --mail-type=ALL
+
+parallel < /home/taghianj/scratch/SAC_GCN/scripts/ComputeCanada/cedar/HalfCheetah/standard.txt
--- scripts/test.sh
+++ scripts/test.sh
@@ -3,20 +3,8 @@ MAIN_FILE="/home/mehran/Documents/SAC_GCN/Controller/basic/main.py"
 
 MAIN_FILE="/local/melco3/taghianj/SAC_GCN/Controller/graph/main.py"
 
-MAIN_FILE="/home/mehran/Documents/SAC_GCN/Controller/graph/main.py"
+MAIN_FILE="/home/taghianj/Documents/SAC_GCN/Controller/graph/main.py"
 
-CUDA_VISIBLE_DEVICES=0 python $MAIN_FILE --env-name FetchReachEnv-v0 --exp-type test --automatic_entropy_tuning True --num_steps 2000000 --start_steps 10000 -dsf 100 --cuda --seed 0
 
-CUDA_VISIBLE_DEVICES=0 python $MAIN_FILE --env-name FetchReachEnv-v2 --exp-type test --automatic_entropy_tuning True --num_steps 300000 --start_steps 5000 -dsf 100 --cuda --seed 0
-
-CUDA_VISIBLE_DEVICES=0 python $MAIN_FILE --env-name FetchReachEnv-v4 --exp-type test --automatic_entropy_tuning True --num_steps 300000 --start_steps 5000 -dsf 100 --cuda --seed 0
-
-CUDA_VISIBLE_DEVICES=0 python $MAIN_FILE --env-name FetchReachEnv-v5 --exp-type test --automatic_entropy_tuning True --num_steps 2000000 --start_steps 10000 -dsf 100 --cuda --seed 0
-
-CUDA_VISIBLE_DEVICES=0 python $MAIN_FILE --env-name FetchPickAndPlaceEnv-v0 --exp-type test --automatic_entropy_tuning True --num_steps 1000000 --start_steps 5000 -dsf 100 --cuda --seed 0
-
-CUDA_VISIBLE_DEVICES=0 python $MAIN_FILE --env-name AntEnv-v0 --exp-type standard --automatic_entropy_tuning True --num_steps 10000000 --start_steps 10000 -dsf 10 --cuda --seed 0 --aggregation sum
-
-CUDA_VISIBLE_DEVICES=0 python $MAIN_FILE --env-name AntEnvGraph-v0 --exp-type standard --automatic_entropy_tuning True --num_episodes 50000 --start_steps 10000 -dsf 10 --cuda --seed 0
-
-CUDA_VISIBLE_DEVICES=0 python $MAIN_FILE --env-name HalfCheetah-v2 --exp-type test --automatic_entropy_tuning True --num_episodes 10000 --start_steps 10000 -dsf 10 --cuda --seed 0
+tmux new-session -d -s walker2d-test "CUDA_VISIBLE_DEVICES=0 python $MAIN_FILE --env-name Walker2d-v2 --exp-type test --automatic_entropy_tuning True --num_episodes 30000 --start_steps 0 -dsf 5 --cuda --seed 0"
+tmux new-session -d -s halfcheetah-test "CUDA_VISIBLE_DEVICES=0 python $MAIN_FILE --env-name HalfCheetah-v2 --exp-type test --automatic_entropy_tuning True --num_episodes 30000 --start_steps 0 -dsf 5 --cuda --seed 0"
