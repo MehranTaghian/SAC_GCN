8aba29be86662a0b3632277b5e9a4e6564fa96b5
mehran <taghianmehran@gmail.com>
2022-06-16 15:13:08 -0600

    CC scripts added for halfcheetah

--- scripts/ComputeCanada/cedar/halfcheetah/standard.sh
+++ scripts/ComputeCanada/cedar/halfcheetah/standard.sh
@@ -4,7 +4,7 @@
 #SBATCH --gpus-per-node=v100:4
 #SBATCH --ntasks-per-node=32
 #SBATCH --exclusive
-#SBATCH --mem=187G
+#SBATCH --mem=185G
 #SBATCH --time=15-00:00
 #SBATCH --job-name="HalfCheetah-15Seeds"
 #SBATCH --account=def-zaiane
