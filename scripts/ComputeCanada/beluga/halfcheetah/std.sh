a25e953d8808208434e28faa6ac93092f5dbff40
mehran <taghianmehran@gmail.com>
2022-06-17 12:38:11 -0600

    saving experimental data updated

--- scripts/ComputeCanada/beluga/halfcheetah/std.sh
+++ scripts/ComputeCanada/beluga/halfcheetah/std.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=v100:4
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=40
 #SBATCH --mem=1G
 #SBATCH --time=00:03:00
-#SBATCH --job-name="HalfCheetah"
+#SBATCH --job-name="information"
 #SBATCH --account=def-zaiane
-#SBATCH --output=HalfCheetah-%j.out
+#SBATCH --output=information-%j.out
 #SBATCH --mail-user=taghianj@ualberta.ca
 #SBATCH --mail-type=ALL
 
