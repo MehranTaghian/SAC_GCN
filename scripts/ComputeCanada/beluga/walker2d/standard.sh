dd305762648ad4a8b80d5f38045f6e64cf0eba33
mehran <taghianmehran@gmail.com>
2022-06-20 14:54:04 -0600

    beluga scripts updated for walker2d

---
+++ scripts/ComputeCanada/beluga/walker2d/standard.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=v100:4
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+#SBATCH --mem=191000M
+#SBATCH --time=7-00:00
+#SBATCH --job-name="HalfCheetah-15Seeds"
+#SBATCH --account=def-zaiane
+#SBATCH --output=HalfCheetah-%j.out
+#SBATCH --mail-user=taghianj@ualberta.ca
+#SBATCH --mail-type=ALL
+
+parallel < /home/taghianj/scratch/SAC_GCN/scripts/ComputeCanada/run/walker2d/standard.txt
