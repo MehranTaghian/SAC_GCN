7231f8826c19e6670b14d8275702bd3e436659ab
mehran <taghianmehran@gmail.com>
2022-06-28 09:45:55 -0600

    fetchreach scripts updated

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -23,3 +23,10 @@ rsync -a taghianj@cedar.computecanada.ca:/home/taghianj/scratch/SAC_GCN/Data/* /
 
 # beluga
 rsync -a taghianj@beluga.computecanada.ca:/home/taghianj/scratch/SAC_GCN/Data/* /home/taghianj/Documents/SAC_GCN/Data/beluga
+
+# MELCO
+# cedar
+rsync -a taghianj@cedar.computecanada.ca:/home/taghianj/scratch/SAC_GCN/Data/* /local/melco3/taghianj/SAC_GCN/Data/cedar
+
+# beluga
+rsync -a taghianj@beluga.computecanada.ca:/home/taghianj/scratch/SAC_GCN/Data/* /local/melco3/taghianj/SAC_GCN/Data/beluga
