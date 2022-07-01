33849806b9e83db529df151b1d0eb77a7499da47
mehran <taghianmehran@gmail.com>
2022-06-30 19:06:24 -0600

    saving buffer data to file updated to async save

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -11,12 +11,16 @@ rsync -a taghianj@mehran.cs.ualberta.ca:/home/taghianj/Documents/SAC_GCN/Data /h
 # cedar
 rsync -a taghianj@cedar.computecanada.ca:/home/taghianj/scratch/SAC_GCN/Data /home/mehran/Documents/SAC_GCN/Data/cedar
 
-############## TAGHIANJ
+##################################### TAGHIANJ
 # MELCO 1
 rsync -a taghianj@melco.cs.ualberta.ca:/local/melco3/taghianj/SAC_GCN/Data /home/taghianj/Documents/SAC_GCN
 
 #MELCO 2
 rsync -a taghianj@melco2.cs.ualberta.ca:/home/taghianj/Documents/SAC_GCN/Data /home/taghianj/Documents/SAC_GCN
+
+# GOOGLE DRIVE
+rsync -a /home/taghianj/Documents/SAC_GCN/Data/ /home/taghianj/google-drive/SAC-GCN/
+
 
 # cedar
 rsync -a taghianj@cedar.computecanada.ca:/home/taghianj/scratch/SAC_GCN/Data/* /home/taghianj/Documents/SAC_GCN/Data/cedar
@@ -24,7 +28,7 @@ rsync -a taghianj@cedar.computecanada.ca:/home/taghianj/scratch/SAC_GCN/Data/* /
 # beluga
 rsync -a taghianj@beluga.computecanada.ca:/home/taghianj/scratch/SAC_GCN/Data/* /home/taghianj/Documents/SAC_GCN/Data/beluga
 
-# MELCO
+##################################### MELCO
 # cedar
 rsync -a taghianj@cedar.computecanada.ca:/home/taghianj/scratch/SAC_GCN/Data/* /local/melco3/taghianj/SAC_GCN/Data/cedar
 
