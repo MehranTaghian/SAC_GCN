5d22a2bf43e9198adc7ce832c7199eb598c084d0
mehran <taghianmehran@gmail.com>
2022-07-20 11:29:31 -0600

    Fetch Reach torque action-space first version added

--- scripts/plots.sh
+++ scripts/plots.sh
@@ -7,6 +7,7 @@ PLOT_FILE=/home/taghianj/Documents/SAC_GCN/Plots/learning_curve.py
 
 
 python $PLOT_MULTIPLE_FILE --env-name FetchReachDense-v1
+python $PLOT_MULTIPLE_FILE --env-name FetchReach-v2
 python $PLOT_MULTIPLE_FILE --env-name FetchReachBroken-v1
 python $PLOT_MULTIPLE_FILE --env-name HalfCheetah-v2
 python $PLOT_MULTIPLE_FILE --env-name HalfCheetahBroken-v2
