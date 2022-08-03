ee44d752e6f4b3a1750f4cf20cabeaab805a5a53
mehran <taghianmehran@gmail.com>
2022-08-02 19:29:51 -0600

    Plot colormap changed to colorblind

--- Plots/multiple_type_learning_curves.py
+++ Plots/multiple_type_learning_curves.py
@@ -6,6 +6,10 @@ import argparse
 import pathlib
 import os
 from scipy.stats import ttest_ind
+import matplotlib.style as style
+
+# style.use('seaborn-colorblind')
+style.use('tableau-colorblind10')
 
 parser = argparse.ArgumentParser(description="Draw results of the experiments inside a directory")
 
@@ -67,18 +71,31 @@ def draw():
 
 
 def plot_learning_curve(experiment_results, x_label, y_label, title):
-    plt.figure(figsize=[12, 9])
-    for type in experiment_results.keys():
+    width = 15
+    height = 12
+    sns.set_theme()
+    sns.set(font_scale=2)
+    fig, ax = plt.subplots(figsize=[width, height])
+    colors = plt.cm.cividis(np.linspace(0, 1, len(experiment_results.keys())))
+    # colors = plt.cm.tab20b(np.linspace(0, 1, len(experiment_results.keys())))
+    # colors = sns.color_palette('colorblind', n_colors=len(experiment_results.keys()))
+    for type, color in zip(experiment_results.keys(), colors):
         x, average, standard_error = experiment_results[type]
-        plt.plot(x[x_label], average, label=type)
-        plt.fill_between(x[x_label], average - 2.26 * standard_error, average + 2.26 * standard_error, alpha=0.2)
-    plt.xlabel(X_AXIS_TO_LABEL[x_label])
-    plt.ylabel(y_label)
-    plt.legend()
-    plt.title(title)
-    plt.savefig(os.path.join(exp_path, x_label + '.jpg'))
-    plt.close()
-
+        ax.plot(x[x_label], average, label=type, color=color, linewidth=3)
+        ax.fill_between(x[x_label], average - 2.26 * standard_error, average + 2.26 * standard_error,
+                         color=color,
+                         alpha=0.2)
+    ax.set_xlabel(X_AXIS_TO_LABEL[x_label])
+    ax.set_ylabel(y_label)
+    legs = fig.legend(ncol=3, fancybox=True, loc='lower center', bbox_to_anchor=(0.6, 0.1))
+    for leg in legs.legendHandles:
+        leg.set_linewidth(10.0)
+    ax.set_title(title)
+    # ax.spines.right.set_visible(False)
+    # ax.spines.top.set_visible(False)
+    fig.tight_layout()
+    fig.savefig(os.path.join(exp_path, x_label + '.jpg'))
+    sns.reset_orig()
 
 def plot_significancy_test(exp_results):
     p_values = np.zeros([len(exp_results), len(exp_results)])
@@ -87,16 +104,19 @@ def plot_significancy_test(exp_results):
         for j, type2 in enumerate(exp_results.keys()):
             p_values[i, j] = ttest_ind(exp_results[type1][1], exp_results[type2][1]).pvalue
 
-    plot_t_test_heatmap(p_values, exp_results.keys())
+    labels = [' '.join(j.split('_')[:-1]).strip() if len(j.split('_')) > 1 else j for j in exp_results.keys()]
+    plot_t_test_heatmap(p_values, labels)
 
 
 def plot_t_test_heatmap(data, labels):
-    width = 12
-    height = 9
+    width = 10
+    height = 10
+    plt.rcParams.update({'font.size': 14})
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(width, height), gridspec_kw={'width_ratios': (30, 1)})
     mask = np.triu(np.ones_like(data))
-    sns.heatmap(data, ax=ax1, cbar=False, cmap="YlGnBu", linewidth=1, vmin=np.min(data), vmax=np.max(data),
+    sns.heatmap(data, ax=ax1, cbar=False, cmap="cividis", linewidth=1, vmin=np.min(data), vmax=np.max(data),
                 annot=True,
+                fmt='.2f',
                 mask=mask)
     ax1.set_xticks(np.arange(len(labels) - 1) + 0.5, labels=list(labels)[:-1], rotation=45)
     ax1.set_yticks(np.arange(len(labels) - 1) + 1.5, labels=list(labels)[1:], rotation=45)
@@ -106,7 +126,8 @@ def plot_t_test_heatmap(data, labels):
     ax1.set_ylabel("Occluded joint name")
     ax1.set_xlabel(f"Occluded joint name")
 
-    plt.colorbar(plt.cm.ScalarMappable(cmap="YlGnBu", norm=plt.Normalize(vmin=np.min(data), vmax=np.max(data))), cax=ax2)
+    plt.colorbar(plt.cm.ScalarMappable(cmap="YlGnBu", norm=plt.Normalize(vmin=np.min(data), vmax=np.max(data))),
+                 cax=ax2)
     ax2.yaxis.set_ticks_position('left')
     ax2.set_ylabel('P values (P < 0.05 means they are statistically significantly different)')
 
